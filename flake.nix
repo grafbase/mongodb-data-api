e13ac7aba03415dc323ebf5eb220c0b301ccdad7
Julius de Bruijn <julius+github@nauk.io>
2023-08-10 16:49:14 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Local MongoDB Atlas Data API";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rustup
+        ];
+      };
+    });
+}
